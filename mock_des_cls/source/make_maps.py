0f9c4fc94e2c66d33e215a5143ae43440448408a
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2014-09-03 22:54:59 +0100

    a code computing pcl for full sky maps added

--- mock_des_cls/source/make_maps.py
+++ mock_des_cls/source/make_maps.py
@@ -5,10 +5,11 @@
 import numpy as np
 import healpy as hp
 import sys
+import time
 
 
 # read the pixel values from Marc's file and create maps
-# The format of the file is 
+# The format of the file is
 # ipix, base err, a1 err, a2 err, a3 err, a4 err, b1 err, b2 err, b3 err, b4 err
 
 NSIDE=128
@@ -29,63 +30,71 @@ headers["b4"]=17
 def read_data(file_name):
 	print "Reading the file",file_name
 	marc_data = np.loadtxt(file_name,skiprows=1)
-	
+
 	return marc_data
 
 def make_maps(marc_data,map_id):
 	"""
 	Make maps by reading the file in the fomat
 	ipix, base err, a1 err, a2 err, a3 err, a4 err, b1 err, b2 err, b3 err, b4 err
-	
+
 	@param map_id the id (string) of the map {base, a1, a2, a3, a4, b1, b2, b3, b4}
 	"""
-	
-	
+
+
 	print "getting data of ",map_id
-	
+
 	# what data do we need?
 	col = headers[map_id]
-	
+
 	# get it
 	data = marc_data[:,col]
 	noise_sigma = marc_data[:,col+1]
-	
+
 	inverse_noise = np.zeros(NPIX)
-	
+
 	inverse_noise[noise_sigma>0] = 1./noise_sigma[noise_sigma>0]**2
-	
+
 	return (data,inverse_noise)
-	
+
 def write_data_and_noise(file_prefix,data,inverse_noise):
 	"""
 	Write the data and iverse noise matrix as a fits file
-	
+
 	@param file_prefix prefix to the files
 	@param data data as a healpix map
 	@param inverse_noise inverse noise as a healpix map
 	"""
-	
+
 	data_file_name = file_prefix + "_data.fits"
 	inv_noise_file_name = file_prefix + "_invNoise.fits"
-	
+
 	print "writing data to file",data_file_name
-	
+
 	hp.write_map(data_file_name,data)
 
 	print "writing inv noise to file",inv_noise_file_name
 
 	hp.write_map(inv_noise_file_name,inverse_noise)
-	
-	
+
+
 #############################
-marc_file_name = "/share/data1/sbalan/Misc/DES_mocks_from_Marc/desmock_test_filetable_0128.dat"
-output_file_prefix = marc_file_name.replace(".dat","_")
+if __name__ == "__main__":
+	if len(sys.argv) == 2 :
+		start_time = time.time()
 
-marc_data=read_data(marc_file_name)
-print ""
+		marc_file_name = sys.argv[1]
+		output_file_prefix = marc_file_name.replace(".dat","_")
 
-for map_id in ["base","a1","a2","a3","a4","b1","b2","b3","b4"]:
-	data,inverse_noise=make_maps(marc_data,map_id)
-	write_data_and_noise(output_file_prefix+map_id,data,inverse_noise)
-	print ""
+		marc_data=read_data(marc_file_name)
+		print ""
 
+		for map_id in ["base","a1","a2","a3","a4","b1","b2","b3","b4"]:
+			data,inverse_noise=make_maps(marc_data,map_id)
+			write_data_and_noise(output_file_prefix+map_id,data,inverse_noise)
+			print ""
+
+		print (time.time() - start_time) / 60.0, 'minutes'
+	else:
+		print "usage: python ",sys.argv[0],"<path-to-catalogue>"
+		print "example: python",sys.argv[0], "/share/data1/sbalan/Misc/DES_mocks_from_Marc/desmock_test_filetable_0128.dat"
---
+++ mock_des_cls/source/simple_pcl.py
@@ -0,0 +1,61 @@
+import numpy as np
+import healpy as hp
+import sys
+import time
+
+def compute_pcl_estimate(data_file,inv_noise_file):
+  d = hp.read_map(data_file)
+  inv_n = hp.read_map(inv_noise_file)
+  n  = 1./np.sqrt(inv_n)
+
+  nside = hp.npix2nside(np.shape(d)[0])
+
+  # compute the total power spectrum
+  C_l = hp.anafast(d,lmax=2*nside)
+
+  num_samps = int(1000)
+
+  N_l = np.zeros(np.shape(C_l))
+
+  # Monte Carlo
+  mu = np.zeros(np.shape(d))
+  for samp in range(num_samps):
+    if samp % 100 == 0 :
+      print "samples =",samp
+    # draw a realisation from noise
+    n_i = np.random.normal(mu,n)
+
+    # find the power spectrum of this realisation
+    N_l_i = hp.anafast(n_i,lmax=2*nside)
+
+    # accumulate
+    N_l += N_l_i
+
+  N_l /= float(num_samps)
+
+  # subtract
+  S_l = C_l - N_l
+
+  return (C_l,N_l,S_l)
+
+def write_pcl(output_file,C_l,N_l,S_l):
+  ell=np.arange(0,np.shape(S_l)[0])
+  np.savetxt(output_file,np.asarray([ell,C_l,N_l,S_l]).T,delimiter=",")
+
+
+if __name__ == "__main__":
+  if len(sys.argv) == 4 :
+    start_time = time.time()
+
+    data_file = sys.argv[1]
+    inv_noise_file =sys.argv[2]
+    output_file = sys.argv[3]
+
+    C_l,N_l,S_l = compute_pcl_estimate(data_file,inv_noise_file)
+    write_pcl(output_file,C_l,N_l,S_l)
+
+    print ""
+    print (time.time() - start_time) / 60.0, 'minutes'
+  else:
+    print "usage: python ",sys.argv[0],"<data> <inv-noise-cov-mat"
+    print "example: python",sys.argv[0], "./data.fits ./invNoise.fits ./base.pcl"
