73b928d9cb3d8da62f01ddfa2c1ab30b7a64d6c5
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2014-09-23 00:28:52 +0100

    spicy is cl estimation script. the back end is spice

---
+++ spicy/README.md
@@ -0,0 +1,11 @@
+# SPICY
+
+A python interface to [spice](http://www2.iap.fr/users/hivon/software/PolSpice/).
+
+## Requirements
+
+An installation of `spice`. The executable `spice` should be in the path.
+Set the environment variable `HEALPIXDATA`. For example in the bas shell you can
+do this by
+
+    $ export HEALPIXDATA=/arxiv/projects/CMB/PolSpice_v03-00-01/external/Healpix_3.11/data
---
+++ spicy/spice_cl.py
@@ -0,0 +1,118 @@
+from subprocess import call
+import matplotlib.pyplot as plt
+import numpy as np
+import healpy as hp
+import sys
+import time
+
+
+spice_exe='spice'
+spice_data='spice_data.fits'
+spice_noise='spice_noise.fits'
+spice_mask='spice_mask.fits'
+spice_dl='spice_dl.dat'
+spice_nl='spice_nl.dat'
+spice_bl='spice_bl.dat'
+
+def get_mask_file(inv_noise_map):
+    mask=np.zeros(np.shape(inv_noise_map))
+    mask[inv_noise_map>0]=1
+
+    return mask
+
+def compute_pcl_estimate(data_file,inv_noise_file,beam_file):
+    #write the data file
+    d = hp.read_map(data_file)
+    hp.write_map(spice_data,m=d)
+
+    #create a mask file from inv_noise
+    inv_n = hp.read_map(inv_noise_file)
+    msk = get_mask_file(inv_n)
+    hp.write_map(spice_mask,m=msk)
+
+    #write the noise map
+    n = np.zeros(np.shape(inv_n))
+    n[inv_n>0]  = 1./np.sqrt(inv_n[inv_n>0])
+
+    #write the beam file
+    B_l_in = np.loadtxt(beam_file,delimiter=",")
+    #np.savetxt(spice_bl,B_l_in,fmt="%.4f")
+    B_l = B_l_in[:,1]
+
+
+    if d.shape != inv_n.shape :
+        raise RuntimeError("data and noise have different dimensions")
+
+    nside=hp.npix2nside(np.shape(d)[0])
+
+    #compute the powe spectrum of the data
+    call([spice_exe,'-mapfile',spice_data,'-maskfile',spice_mask,'-clfile',
+        spice_dl,'-corfile','NO','-nlmax',str(2*nside),'-verbosity','NO'])#'-beam_file',spice_bl,
+    call(['rm',spice_data])
+
+
+    #read the power spectrum
+    D_l = np.loadtxt(spice_dl,skiprows=1)[:,1]
+
+    #apply beam to the cls
+    D_l /= B_l**2
+
+    #compute the noise power spectrum using Monte Carlo
+    N_l = np.zeros(np.shape(D_l))
+    mu = np.zeros(np.shape(d))
+    num_samps = int(1000)
+    for samp in range(num_samps):
+        if samp % 100 == 0 :
+            print "samples taken =",samp
+        # draw a realisation from noise
+        n_i = np.random.normal(mu,n)
+        #write this to file
+        hp.write_map(spice_noise,m=n_i)
+
+        # find the power spectrum of this realisation
+        call([spice_exe,'-mapfile',spice_noise,'-maskfile',spice_mask,'-clfile',
+            spice_nl,'-corfile','NO','-nlmax',str(2*nside),'-verbosity','NO'])
+
+        #read the power spectrum
+        N_l_i = np.loadtxt(spice_nl,skiprows=1)[:,1]
+
+        # accumulate
+        N_l += N_l_i
+
+        #delete the noise realisation
+        call(['rm',spice_noise])
+
+    N_l /= float(num_samps)
+
+    #apply beam to the nls
+    N_l /= B_l**2
+
+    # subtract
+    S_l = D_l - N_l
+
+    #delete the mask
+    call(['rm',spice_mask])
+
+    return (D_l,N_l,S_l)
+
+def write_pcl(output_file,C_l,N_l,S_l):
+  ell=np.arange(0,np.shape(S_l)[0])
+  np.savetxt(output_file,np.asarray([ell,C_l,N_l,S_l]).T,delimiter=",")
+
+if __name__ == "__main__":
+  if len(sys.argv) == 5 :
+    start_time = time.time()
+
+    data_file = sys.argv[1]
+    inv_noise_file =sys.argv[2]
+    beam_file = sys.argv[3]
+    output_file = sys.argv[4]
+
+    C_l,N_l,S_l = compute_pcl_estimate(data_file,inv_noise_file,beam_file)
+    write_pcl(output_file,C_l,N_l,S_l)
+
+    print ""
+    print (time.time() - start_time) / 60.0, 'minutes'
+  else:
+    print "usage: python ",sys.argv[0],"<data> <inv-noise-cov-mat> <beam-file> <output-cl-file>"
+    print "example: python",sys.argv[0], "./data.fits ./invNoise.fits ./window_func_temp_ns128.bl ./base.pcl "
