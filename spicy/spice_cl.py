a4d3aee4b1797fecf76b89e56806b7f96dfa8495
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-01-28 23:43:16 +0000

    counts/weights cl computation method added

--- spicy/spice_cl.py
+++ spicy/spice_cl.py
@@ -20,6 +20,120 @@ def get_mask_file(inv_noise_map):
 
     return mask
 
+
+
+def compute_pcl_estimate_with_weights(counts_file,weight_file,beam_file,apodize=False):
+    """
+    Compute the power spectrum from counts map and mask
+
+    @param counts_file a healpix map with galaxy counts
+    @param mask_file a healpix map with w
+    """
+    #read the data
+    d = hp.read_map(counts_file)
+
+    #read the weights file
+    weight = hp.read_map(weight_file)
+
+    if d.shape != weight.shape :
+        raise RuntimeError("data and weights have different dimensions")
+
+    #force all the values < 0 in weights to zero
+    weight[weight<0] = 0.
+
+    #compute the useful area
+    useful_pixels = len(weight[weight>0])
+
+    fsky = useful_pixels/float(np.shape(weight)[0])
+
+    print "fsky =",fsky
+
+    #now apply the mask
+    d *= weight
+
+    total_objects = np.sum(d[weight>0])
+    print "total objects = ", total_objects
+
+
+    N_bar = total_objects/float(useful_pixels) #only some pixels are useful
+    print "N_bar = ",N_bar
+
+    #make the data over-density
+    d = (d-N_bar)/N_bar
+
+    #make the values outside the mask to zero
+    d[weight <= 0] = 0.
+
+    shot_noise = 4*np.pi*fsky/total_objects
+    print "shot noise = ", shot_noise
+
+    #write data
+    hp.write_map(spice_data,m=d)
+
+    #create a mask file from weights
+    msk = get_mask_file(weight)
+
+    #write mask
+    hp.write_map(spice_mask,m=msk)
+
+    nside=hp.npix2nside(np.shape(d)[0])
+
+    #compute the power spectrum of the mask
+    call([spice_exe,'-mapfile',spice_mask,'-corfile',spice_crr])
+
+    #load the crr file
+    W_t_in = np.loadtxt(spice_crr,skiprows=1)
+    theta = W_t_in[:,0]
+    W_t = W_t_in[:,2]
+
+    if apodize :
+        ap_sigma = np.max(theta[np.log10(np.abs(W_t))>-2.5])*180./np.pi
+
+        if ap_sigma <= 0.:
+            raise RuntimeError("ap_sigma should be >0")
+
+        print "ap sigma = ",ap_sigma
+
+
+    #compute the powe spectrum of the data
+    if apodize :
+        call([spice_exe,'-mapfile',spice_data,'-maskfile',spice_mask,'-clfile',
+            spice_dl,'-corfile','NO','-nlmax',str(2*nside),'-verbosity','NO',
+            '-thetamax', str(ap_sigma+0.1), '-apodizesigma', str(ap_sigma)])
+    else:
+        call([spice_exe,'-mapfile',spice_data,'-maskfile',spice_mask,'-clfile',
+            spice_dl,'-corfile','NO','-nlmax',str(2*nside),'-verbosity','NO'])
+
+    #delete data
+    call(['rm',spice_data])
+
+
+    #read the power spectrum
+    D_l = np.loadtxt(spice_dl,skiprows=1)[:,1]
+
+    N_l = np.ones(np.shape(D_l))*shot_noise
+
+    #load the beam file
+    B_l_in = np.loadtxt(beam_file,delimiter=",")
+    B_l = B_l_in[:,1]
+
+    #apply beam to the nls
+    N_l /= B_l**2
+
+    # subtract
+    S_l = D_l - N_l
+
+    #delete the mask
+    call(['rm',spice_mask])
+    call(['rm',spice_crr])
+    call(['rm',spice_dl])
+
+    return (D_l,N_l,S_l)
+
+
+
+
+
 def compute_pcl_estimate(data_file,inv_noise_file,beam_file,num_samps,apodize=False):
     #write the data file
     d = hp.read_map(data_file)
